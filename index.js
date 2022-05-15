1bec10852f107dcf9f44eb4781c29864b484cc67
Nashtarin Nur <nashtarin.nur@gmail.com>
2022-05-15 17:43:49 +0600

    working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const app=express()
 // const { MongoClient } = require('mongodb');
+const ObjectId=require('mongodb').ObjectId;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
@@ -11,6 +12,9 @@ const port=process.env.PORT||5000
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rf3gx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// const bodyParser = require('body-parser');
+
+// app.use(bodyParser());
 // client.connect(err => {
 //   const collection = client.db("test").collection("devices");
 //   // perform actions on the collection object
@@ -23,10 +27,33 @@ async function run(){
     try {
         await client.connect();
         const database = client.db('translatorRobberLanguage');
+        const inputEnglishTextCollection=database.collection('normal');
+        const translatedRövarspråkCollection=database.collection('rovarsprak')
+        app.get('/normal',async(req,res)=>{
+            const cursor=inputEnglishTextCollection.find({});
+            const text=await cursor.toArray();
+            res.send(text)
+        })
+        app.get('/normal/:textId',async(req,res)=>{
+            const id=req.params.offerId;
+        const query={_id:ObjectId(id),
+                   };
+        const text=await inputEnglishTextCollection.findOne(query);
+        res.json(query)
+        })
+        app.post('/translate/normal', async (req, res) => {
+            const textInput = req.body;
+               const result = await inputEnglishTextCollection.insertOne(textInput);
+               res.json(result);})
+     
+  
+        
+  
+
         console.log('Database Connected Successfully')
     
 } finally {
-    await client.close();
+   
 
 }}
 run().catch(console.dir)
